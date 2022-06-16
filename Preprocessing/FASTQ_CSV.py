e7964c6b03b1885ce97032f4167fd3d45fab58d6
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-06-16 01:05:18 -0600

    Convert FASTQ to CSV

---
+++ Preprocessing/FASTQ_CSV.py
@@ -0,0 +1,40 @@
+
+
+import pandas as pd
+
+def FASTA2DF (file):
+    # The function takes the FASTA file, convert it 2-column dataframe (one for the ID, one for the read)
+    
+    # open file and iterate through the lines, composing each single line as we go
+    id_lines = []
+    seq_lines = []
+    with open(file) as fp:
+         for line in fp:
+             if line.startswith('@'):
+                 id_lines.append(line)
+             else:
+                 line.replace('\n', "")
+                 seq_lines.append(line)
+    
+    # Calling DataFrame constructor after zipping
+    # both lists, with columns specified
+    df = pd.DataFrame(list(zip(id_lines, seq_lines)),
+                   columns =['id', 'read']).replace(r'\n','', regex=True)
+    
+    #Calculates the length of each read, append the length
+    df1= df.read.str.len().tolist()
+    df1 = pd.Series(df1 , name = 'length')
+    
+    
+    #Sort the reads descendingly
+    df2 = pd.concat([df, df1], axis=1, join="inner").sort_values("length",ascending=False)
+    df2.reset_index(drop=True, inplace=True)
+    
+        
+    return df2
+
+
+        
+if __name__ == "__main__":
+    df = FASTA2DF("./Raw_Data/sample_data.fastq")    
+    df.to_csv("./Processed_data/data_Viral.csv")
