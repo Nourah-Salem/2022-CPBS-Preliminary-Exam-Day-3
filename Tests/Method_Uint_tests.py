976990ce3e2f8ec05a46212dc65924a5f5bf066c
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-06-16 02:56:12 -0600

    Update Method_Uint_tests.py

--- Tests/Method_Uint_tests.py
+++ Tests/Method_Uint_tests.py
@@ -1,8 +1,8 @@
 
 
-from EDA_sample_prep import FASTA2DF
-from Kmer_builder import count_kmers
-from Kmer_builder import PCA 
+from FASTA_CSV import FASTA2DF
+from PCA_of_Kmer_Frequency import count_kmers
+from PCA_of_Kmer_Frequency import PCA 
 
 import unittest
 import pandas as pd
@@ -13,6 +13,8 @@ import numpy as np
 class project_tests(unittest.TestCase):
     
     def test_FASTA2DF_return(self):
+        
+        '''this test function tests the correctness of the dataframe generated from FASTA (and the same goes for the FASTQ function), it tests the type of the columns that were generated from FASTA file'''
         
         # expected
          
@@ -27,7 +29,7 @@ class project_tests(unittest.TestCase):
             'length': [int]}) '''
         
         #actual
-        df = FASTA2DF("D:/PhD at Anschutz/semester2/7712/prelim/data2.fasta")
+        df = FASTA2DF("D:/PhD at Anschutz/semester2/7712/prelim/bacterial_data.fasta")
         
         # assert
         assert ptypes.is_string_dtype(df['id'])
@@ -38,13 +40,18 @@ class project_tests(unittest.TestCase):
     
     def read_char_check(self):
         
-        non_DNA = re.compile('[QWERYUIOP{}SDFHJKL:"?><MNBVXZ@_!#$%^&*()<>?/\|}{~:]')
-        df = FASTA2DF("D:/PhD at Anschutz/semester2/7712/prelim/data2.fasta")
+        ''' this test function tests if the reads have any character other than A, T, C, G'''
         
+        non_DNA = re.compile('[QWERYUIOP{}SDFHJKL:"?><MNBVXZ@_!#$%^&*()<>?/\|}{~:]')
+        df = FASTA2DF("D:/PhD at Anschutz/semester2/7712/prelim/bacterial_data.fasta")
+        
+        # chech if the reads have any og the characters in the non_DNA list
         if(non_DNA.search(df['read'][1]) == None):
+            # generate True variable of a read has any special char
             X = True
          
         else:
+            # generate False variable of a read doesn't have any special char
             X = False
             
         self.assertTrue(X)
@@ -53,6 +60,8 @@ class project_tests(unittest.TestCase):
 
     
     def num_kmer_generated(self):
+        
+        '''this test function checks the number of the kmers generated from out Kmer builder function the correct number of the kmer depend on have only the DNA sequence, if there is any additional special character (e.g "{"), the test will fail'''
         
         read = 'AATTCCGGAA'
         k= 8
@@ -73,8 +82,11 @@ class project_tests(unittest.TestCase):
     
     def PCA_input_output_same_shape_check(self):
         
+        '''this test function checks if the shape of the input to the PCA is correct or not.
+        Here, we compare the shape of the PCA input to an idea shpae that we generated from random data'''
+        
         # expected 
-        input_shape = np.random.randint(10,50,60).reshape(20,3) 
+        input_shape = np.random.randint(10,50,60).reshape(50,2) 
         
         output_shape = PCA(input_shape)
         
